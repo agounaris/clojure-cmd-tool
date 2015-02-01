49f73368254b747f69cb0873805f9285a94ac392
Argyrios Gounaris <agounaris@gmail.com>
2015-02-01 00:17:14 +0000

    fixing licence

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject command-line-tool "0.0.1"
   :description "Command line tool template"
   :url "https://github.com/agounaris/clojure-cmd-tool"
-  :license {:name "BSD License"
-            :url "http://choosealicense.com/licenses/bsd-2-clause/"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]]
   :main command-line-tool.core)
